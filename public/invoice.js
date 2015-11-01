b98ece1f72dec16386ed9461abce077dfa3cc317
Andrea Caldera <andrea.caldera@gmail.com>
2015-11-01 11:59:25 +0000

    add / remove invoice items

--- public/invoice.js
+++ public/invoice.js
@@ -1,3 +1,9 @@
-$('button.add-invoice-item').click(function() {
-    $($('.invoice-item-row')[0]).after($('.invoice-item-row')[0].outerHTML);
+$('.add-invoice-item').click(function () {
+    var newItem = $('.invoice-item-row').clone();
+    newItem.append('<div class="col-sm-1"><span class="remove-invoice-item glyphicon glyphicon-minus red" aria-hidden="true"></span></div>');
+    $('.add-invoice-item-block').before(newItem[0].outerHTML);
+});
+
+$('.remove-invoice-item').click(function (event) {
+    $(event.target).parent().parent().remove();
 });
