a1ccff0ae5cc0e1716b366a841f7516946fc6a6c
Andrea Caldera <andrea.caldera@gmail.com>
2015-11-08 22:53:39 +0000

    gulp tasks

--- README.md
+++ README.md
@@ -14,17 +14,21 @@ Create invoices in PDF format based on an HTML template.
 
 `gulp test`
 
+### Start development
+
+This command starts MongoDB and bootstraps the Node application using Nodemon. 
+
+`gulp start-dev`
+
 ### Start MongoDB
+
+__MongoDB will be running already by the start-dev gulp task.__
 
 `gulp start-mongo`
 
 ### Start the application
 
 `gulp start-app`
-
-### Start the application with nodemon
-
-`gulp start-app-dev`
 
 ## Production
 
--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,16 @@ var nodemon = require('gulp-nodemon');
 var exec = require('child_process').exec;
 var mocha = require('gulp-mocha');
 
+var logging = false;
+
 function runCommand(command) {
     return function (cb) {
-        exec(command, function (err, stdout, stderr) {
-            console.log(stderr);
-            cb(err);
+        exec(command, function (error, stdout, stderr) {
+            if (logging) {
+                console.error(stderr);
+                console.log(stdout);
+            }
+            cb(error);
         });
     }
 }
@@ -36,3 +41,5 @@ gulp.task('start-mongo', runCommand('rm -fr /tmp/invoice-data && mkdir /tmp/invo
 gulp.task('stop-mongo', runCommand('mongo --eval "db.getSiblingDB(\'admin\').shutdownServer()"'));
 
 gulp.task('test', ['start-mongo', 'start-app', 'mochaTest', 'stop-mongo']);
+
+gulp.task('start-dev', ['start-mongo', 'start-app-dev']);
