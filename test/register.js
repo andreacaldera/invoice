e1014097c899d2a071338dc5b2fc0ac9a9a05b00
Andrea Caldera <andrea.caldera@gmail.com>
2015-11-18 18:01:37 +0000

    async in register test

--- test/register.js
+++ test/register.js
@@ -1,5 +1,6 @@
 const Browser = require('zombie');
 var assert = require('assert');
+var async = require('async');
 var testSetup = require('./test-setup');
 
 Browser.localhost('localhost', 8080);
@@ -9,30 +10,59 @@ describe('A user with no login access', function () {
 
     before('setup', testSetup.applicationStartUp);
 
-    it('should be able to register and login', function (done) {
+    function homePage(callback) {
         browser.visit("/", function () {
             assert.ok(browser.success);
-            browser.click('#register-link', function () {
-                assert.ok(browser.success);
-                browser.assert.url({pathname: '/register'});
-                var email = 'new-user@email.com';
-                var password = 'Password123';
-                browser.fill('email', email);
-                browser.fill('password', password);
-                browser.pressButton('register', function() {
-                    assert.ok(browser.success);
-                    browser.assert.url({pathname: '/login'});
-                    browser.fill('email', email);
-                    browser.fill('password', password);
-                    browser.click('#login-button', function() {
-                        assert.ok(browser.success);
-                        browser.assert.url({pathname: '/'});
-                        done();
-                    });
-                });
-
-            });
+            callback();
         });
+    }
+
+    function registerPage(callback) {
+        browser.click('#register-link', function () {
+            assert.ok(browser.success);
+            browser.assert.url({pathname: '/register'});
+            callback();
+        });
+    }
+
+    function register(email, password, callback) {
+        browser.fill('email', email);
+        browser.fill('password', password);
+        browser.pressButton('register', function () {
+            assert.ok(browser.success);
+            browser.assert.url({pathname: '/login'});
+            callback();
+        });
+    }
+
+    function login(email, password, callback) {
+        browser.fill('email', email);
+        browser.fill('password', password);
+        browser.click('#login-button', function () {
+            assert.ok(browser.success);
+            browser.assert.url({pathname: '/'});
+            callback();
+        });
+    }
+
+    it('should be able to register and login', function (done) {
+        var email = 'new-user@email.com';
+        var password = 'Password123';
+        async.series([
+                homePage,
+                registerPage,
+                function (callback) {
+                    register(email, password, callback)
+                },
+                function(callback) {
+                    login(email, password, callback);
+                }
+            ],
+            function (error) {
+                if (error) throw error;
+                done()
+            }
+        );
     });
 
 });
