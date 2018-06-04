263336902d7d7a24c77c54b4e7a64b6b6eac273f
Andrea Caldera <andrea.calderagmail.com>
2018-06-04 21:43:10 +0100

    various changes

--- src/common/modules/meta/reducers.js
+++ src/common/modules/meta/reducers.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 
 module.exports = combineReducers({
+  theme: (state = null) => state,
   featureToggles: (state = []) => state,
   downloadInvoice: (state = []) => state,
 });
