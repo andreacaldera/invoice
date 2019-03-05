d5674db31f52a27870cdb29b2b5831adcd461558
Andrea Caldera <andrea.caldera@gmail.com>
2019-03-05 22:27:28 +0000

    more release changes

--- release/prepare-release.sh
+++ release/prepare-release.sh
@@ -9,13 +9,13 @@ echo "=== PREPARING RELEASE $NEW_VERSION $RELEASE_BRANCH ==="
 
 echo "Updating Jenkinsfile"
 TARGET_BRANCH=$RELEASE_BRANCH node release/update-jenkinsfile.js
-git commit -am "pre-release steps for $NEW_VERSION: jenkinsfile"
+git commit -am "Release $NEW_VERSION: jenkinsfile"
 git push
 
 echo "Preparing release branch"
 git checkout -b $RELEASE_BRANCH
 npm version $NEW_VERSION -no-git-tag-version
-git commit -am "pre-release steps for $NEW_VERSION: package version"
+git commit -am "Release $NEW_VERSION: update package version"
 git push origin $RELEASE_BRANCH
 
 echo "Release preparation completed"
--- release/release.sh
+++ release/release.sh
@@ -4,7 +4,10 @@ RELEASE_BRANCH=$(git branch | grep \\* | cut -d " " -f2)
 echo "=== RELEASING $RELEASE_BRANCH ==="
 
 git checkout master && git pull
-
 git rebase $RELEASE_BRANCH
+git push
 
+echo "Updating Jenkinsfile"
+TARGET_BRANCH="master" node release/update-jenkinsfile.js
+git commit -am "Restore jenkinsfile with master"
 git push
