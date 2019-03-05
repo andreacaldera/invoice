fc48fbce73470aa02c75cff266ef1728f802d0f0
Andrea Caldera <andrea.caldera@gmail.com>
2019-03-05 22:10:00 +0000

    release script info

--- release/prepare-release.sh
+++ release/prepare-release.sh
@@ -5,16 +5,17 @@
 NEW_VERSION=$(node release/get-new-version.js)
 RELEASE_BRANCH=v$NEW_VERSION
 
+echo "=== RELEASE $NEW_VERSION $RELEASE_BRANCH ==="
+
+echo "Updating Jenkinsfile"
 TARGET_BRANCH=$RELEASE_BRANCH node release/update-jenkinsfile.js
+git commit -am "pre-release steps for $NEW_VERSION: jenkinsfile"
+git push
 
-git commit -m "pre-release steps for $NEW_VERSION: jenkinsfile"
-
+echo "Preparing release branch"
 git checkout -b $RELEASE_BRANCH
-
 npm version NEW_VERSION -no-git-tag-version
-
-git commit -m "pre-release steps for $NEW_VERSION: package version"
-
+git commit -am "pre-release steps for $NEW_VERSION: package version"
 git push origin $RELEASE_BRANCH
 
-
+echo "Release preparation completed"
