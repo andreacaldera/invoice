d0be51d2cdea0d38f68f2beca20753d40ee0b815
Andrea Caldera <andrea.caldera@gmail.com>
2019-03-06 13:48:20 +0000

    update script

--- release/release.sh
+++ release/release.sh
@@ -6,13 +6,15 @@ echo "=== RELEASING $RELEASE_BRANCH ==="
 git checkout master && git pull
 git merge $RELEASE_BRANCH
 if [ $? -ne 0 ]; then
-  echo "Merged fails, please fix all conflicts manually before releasing"  
+  echo "\nMerged fails, please fix all conflicts manually before releasing\n"  
   exit 1
 fi
 
 git push
 
-echo "Updating Jenkinsfile"
+echo "\nUpdating Jenkinsfile\n"
 TARGET_BRANCH="master" node release/update-jenkinsfile.js
 git commit -am "Restore jenkinsfile with master"
 git push
+
+echo "\nRelease completed successfully\n"
